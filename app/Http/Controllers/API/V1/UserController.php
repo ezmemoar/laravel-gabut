9caf7e6d4d50f02d2941c2f13bd8a64fbdca835e
Muhamad Rizal Noor Pratama <rizalpratama10010@gmail.com>
2024-03-19 23:58:56 +0700

    feat: install breeze and blade

---
+++ app/Http/Controllers/API/V1/UserController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        return $request->route()->named('user.store');
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Request $request, string $id)
+    {
+        return request()->route()->named('user');
+        return User::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
